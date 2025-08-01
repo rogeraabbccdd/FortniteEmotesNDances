46aa0411b5283975b7aaf8fc355f5906c1f7067f
Sachin <sachinfnatic@gmail.com>
2025-08-02 00:08:18 +0530

    update: re-add Transmit code fix: gamedata sigs & offsets. thanks to @qstage

--- Plugin/Transmit.cs
+++ Plugin/Transmit.cs
@@ -1,14 +1,21 @@
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
 
 namespace FortniteEmotes;
 public partial class Plugin
 {
+    //Client Crash Fix From: https://github.com/qstage/CS2-HidePlayers
+    private static readonly MemoryFunctionVoid<CCSPlayerPawn, CSPlayerState> StateTransition = new(GameData.GetSignature("StateTransition"));
+    private readonly INetworkServerService networkServerService = new();
+
     public void Transmit_OnLoad()
     {
         if(Config.EmoteHidePlayers != 0)
         {
             RegisterListener<Listeners.CheckTransmit>(Hook_CheckTransmit);
+            StateTransition.Hook(Hook_StateTransition, HookMode.Post);
         }
     }
 
@@ -17,7 +24,43 @@ public partial class Plugin
         if(Config.EmoteHidePlayers != 0)
         {
             RemoveListener<Listeners.CheckTransmit>(Hook_CheckTransmit);
+            StateTransition.Unhook(Hook_StateTransition, HookMode.Post);
         }
+    }
+
+    [GameEventHandler(HookMode.Pre)]
+    public HookResult OnPlayerDeathPre(EventPlayerDeath @event, GameEventInfo @info)
+    {
+        CCSPlayerController? player = @event.Userid;
+        if (player != null && player.IsValid)
+        {
+            player.DesiredObserverMode = (int)ObserverMode_t.OBS_MODE_ROAMING;
+            Server.NextFrame(() =>
+            {
+                if (player != null && player.IsValid)
+                {
+                    player.ObserverPawn.Value!.ObserverServices!.ForcedObserverMode = true;
+                    player.ObserverPawn.Value!.ObserverServices!.ObserverMode = (byte)ObserverMode_t.OBS_MODE_ROAMING;
+                    player.ObserverPawn.Value!.ObserverServices!.ObserverLastMode = ObserverMode_t.OBS_MODE_ROAMING;
+                }
+            });
+        }
+        return HookResult.Continue;
+    }
+
+    private HookResult Hook_StateTransition(DynamicHook hook)
+    {
+        var player = hook.GetParam<CCSPlayerPawn>(0).OriginalController.Value;
+        var state = hook.GetParam<CSPlayerState>(1);
+
+        if (player is null) return HookResult.Continue;
+
+        if (state != player.Pawn.Value?.As<CCSPlayerPawnBase>().PlayerState)
+        {
+            ForceFullUpdate(player);
+        }
+
+        return HookResult.Continue;
     }
 
     private void Hook_CheckTransmit(CCheckTransmitInfoList infoList)
@@ -70,5 +113,14 @@ public partial class Plugin
                 }
             }
         }
+    }
+    private void ForceFullUpdate(CCSPlayerController? player)
+    {
+        if (player is null || !player.IsValid) return;
+
+        var networkGameServer = networkServerService.GetIGameServer();
+        networkGameServer.GetClientBySlot(player.Slot)?.ForceFullUpdate();
+
+        player.PlayerPawn.Value?.Teleport(null, player.PlayerPawn.Value.EyeAngles, null);
     }
 }
